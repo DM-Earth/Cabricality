b57352f9c657aa5850d5f9f0e89b1ca97c1b0bd5
Yjn024 <jiening.yu@outlook.com>
2022-07-30 18:18:09 +0800

    scripts

--- kubejs/server_scripts/recipe.js
+++ kubejs/server_scripts/recipe.js
@@ -632,6 +632,42 @@ function initZincMachine(event) {
 	zinc_machine(ED("controller"), 1, ED("connector"))
 }
 
+function initObsidianMachine(event) {
+	event.shaped(KJ("obsidian_machine"), [
+		"SSS",
+		"SCS",
+		"SSS"
+	], {
+		C: CR("railway_casing"),
+		S: KJ("sturdy_mechanism")
+	})
+
+	let transitional = "kubejs:incomplete_sturdy_mechanism"
+	event.recipes.createSequencedAssembly([
+		"kubejs:sturdy_mechanism",
+	], "create:precision_mechanism", [
+		event.recipes.createDeploying(transitional, [transitional, CR("sturdy_sheet")]),
+		event.recipes.createDeploying(transitional, [transitional, CR("sturdy_sheet")]),
+		event.recipes.createPressing(transitional, transitional)
+	]).transitionalItem(transitional)
+		.loops(1)
+		.id("kubejs:sturdy_mechanism")
+	
+	let obsidian_machine = (id, amount, other_ingredient) => {
+		event.remove({ output: id })
+		if (other_ingredient) {
+			event.smithing(Item.of(id, amount), "kubejs:obsidian_machine", other_ingredient)
+			event.recipes.createMechanicalCrafting(Item.of(id, amount), "AB", { A: "kubejs:obsidian_machine", B: other_ingredient })
+		}
+		else
+			event.stonecutting(Item.of(id, amount), "kubejs:obsidian_machine")
+	}
+	obsidian_machine(CR("track_station"), 1, MC("compass"))
+	obsidian_machine(CR("track_signal"), 1, CR("electron_tube"))
+	obsidian_machine(CR("track_observer"), 1, MC("observer"))
+	obsidian_machine(CR("controls"), 1, MC("lever"))
+}
+
 function initInvarMachine(event) {
 	let chop = (type, output) => {
 		event.custom({
@@ -1156,6 +1192,7 @@ onEvent("recipes", event => {
 	initCopperMachine(event)
 	initBrassMachine(event)
 	initZincMachine(event)
+	initObsidianMachine(event)
 	initInvarMachine(event)
 	algalAndesite(event)
 	initEnderMachine(event)
