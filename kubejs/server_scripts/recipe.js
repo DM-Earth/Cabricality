c62a4f0cf8e471dcc09ff32b2e60a9188579b0c4
Yjn024 <jiening.yu@outlook.com>
2022-08-06 10:06:53 +0800

    fix https://github.com/JieningYu/Cabricality/issues/6

--- kubejs/server_scripts/recipe.js
+++ kubejs/server_scripts/recipe.js
@@ -336,6 +336,31 @@ function alloying(event) {
 	event.remove({ id: TC('smeltery/casting/scorched/foundry_controller') })
 	donutCraft(event, TC('foundry_controller'), TC('scorched_bricks'), KJ('infernal_mechanism'))
 	event.remove({ id: TC('smeltery/melting/soul/sand') })
+
+	event.custom({
+		"type": "tconstruct:melting",
+		"ingredient": {
+			"item": "minecraft:redstone"
+		},
+		"result": {
+			"fluid": "kubejs:redstone",
+			"amount": 9000
+		},
+		"temperature": 250,
+		"time": 15
+	})
+	event.custom({
+		"type": "tconstruct:melting",
+		"ingredient": {
+			"item": "minecraft:redstone_block"
+		},
+		"result": {
+			"fluid": "kubejs:redstone",
+			"amount": 81000
+		},
+		"temperature": 250,
+		"time": 135
+	})
 }
 
 function trickierWindmills(event) {
