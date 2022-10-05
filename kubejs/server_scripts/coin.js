71d9ba28b1c720636e2582ca2355f71dfa8f0f14
IcedDog <34804287+IcedDog@users.noreply.github.com>
2022-10-05 09:40:16 +0800

    change things
    just to organise things to make the repo available directly in my minecraft launcher so that i dont have to memorise what ive changed when i commit

--- kubejs/server_scripts/coin.js
+++ kubejs/server_scripts/coin.js
@@ -1,69 +1,70 @@
 function playsound_coin(server, player, type) {
-	if (type == 'silver') {
-		server.runCommandSilent(`playsound minecraft:block.amethyst_cluster.break ambient @a ${player.x} ${player.y} ${player.z}`)
-		server.runCommandSilent(`playsound minecraft:block.anvil.place ambient @a ${player.x} ${player.y} ${player.z} 0.7 1.7`)
-		server.runCommandSilent(`playsound ad_astra:passing_spaceship ambient @a ${player.x} ${player.y} ${player.z} 0.3`)
-	}
+    if (type == 'silver') {
+        server.runCommandSilent(`playsound minecraft:block.amethyst_cluster.break ambient @a ${player.x} ${player.y} ${player.z}`)
+        server.runCommandSilent(`playsound minecraft:block.anvil.place ambient @a ${player.x} ${player.y} ${player.z} 0.7 1.7`)
+        server.runCommandSilent(`playsound ad_astra:passing_spaceship ambient @a ${player.x} ${player.y} ${player.z} 0.3`)
+    }
 
-	if (type == 'gold') {
-		server.runCommandSilent(`playsound minecraft:block.amethyst_block.break ambient @a ${player.x} ${player.y} ${player.z}`)
-		server.runCommandSilent(`playsound minecraft:block.lodestone.place ambient @a ${player.x} ${player.y} ${player.z}`)
-		server.runCommandSilent(`playsound ad_astra:passing_spaceship ambient @a ${player.x} ${player.y} ${player.z} 0.3`)
-	}
+    if (type == 'gold') {
+        server.runCommandSilent(`playsound minecraft:block.amethyst_block.break ambient @a ${player.x} ${player.y} ${player.z}`)
+        server.runCommandSilent(`playsound minecraft:block.lodestone.place ambient @a ${player.x} ${player.y} ${player.z}`)
+        server.runCommandSilent(`playsound ad_astra:passing_spaceship ambient @a ${player.x} ${player.y} ${player.z} 0.3`)
+    }
 }
 
 var coinCooling = []
 
 onEvent('item.right_click', event => {
-	let player = event.getEntity()
-	let messagePrefix = Text.translate(`event.cabricality.coin_flip`, player.getName(), event.getItem().getName()).getString()
+    let player = event.getEntity()
 
-	if (event.getItem() == 'cabricality:silver_coin'
-	&& !coinCooling.includes(player.toString())) {
-		coinCooling.push(player.toString())
 
-		let side = Math.round(Math.random())
-		let coinSide = `top`
-		let message = `§8[ §f§l▲§8 ]`
+    if (event.getItem() == 'cabricality:silver_coin' &&
+        !coinCooling.includes(player.toString())) {
+        coinCooling.push(player.toString())
+        let side = Math.round(Math.random())
+        let coinSide = `top`
+        let message = `§8[ §f§l▲§8 ]`
 
-		if (side == 0) {
-			coinSide = `bottom`
-			message = `§8[ §7§l▼§8 ]`
+        if (side == 0) {
+            coinSide = `bottom`
+            message = `§8[ §7§l▼§8 ]`
 
-		}
+        }
 
-		Minecraft.getInstance().gameRenderer.displayItemActivation(`cabricality:silver_coin_${coinSide}`)
-		playsound_coin(event.server, player, 'silver')
+        Minecraft.getInstance().gameRenderer.displayItemActivation(`cabricality:silver_coin_${coinSide}`)
+        playsound_coin(event.server, player, 'silver')
 
-		event.server.scheduleInTicks(12, event.server, () => {
-			Minecraft.getInstance().gui.getChat().addMessage(messagePrefix + message)
-			event.server.scheduleInTicks(29, event.server, () => {
-				coinCooling = coinCooling.filter(function(item) { return item != player.toString() })
-			})
-		})
-	}
+        event.server.scheduleInTicks(12, event.server, () => {
+            let messagePrefix = Text.translate(`event.cabricality.coin_flip`, player.getName(), event.getItem().getName()).getString()
+            Minecraft.getInstance().gui.getChat().addMessage(messagePrefix + message)
+            event.server.scheduleInTicks(29, event.server, () => {
+                coinCooling = coinCooling.filter(function(item) { return item != player.toString() })
+            })
+        })
+    }
 
-	if (event.getItem() == 'cabricality:gold_coin'
-	&& !coinCooling.includes(player.toString())) {
-		coinCooling.push(player.toString())
+    if (event.getItem() == 'cabricality:gold_coin' &&
+        !coinCooling.includes(player.toString())) {
+        coinCooling.push(player.toString())
 
-		let side = Math.round(Math.random())
-		let coinSide = `top`
-		let message = `§8[ §e§l▲§8 ]`
+        let side = Math.round(Math.random())
+        let coinSide = `top`
+        let message = `§8[ §e§l▲§8 ]`
 
-		if (side == 0) {
-			coinSide = `bottom`
-			message = `§8[ §6§l▼§8 ]`
-		}
+        if (side == 0) {
+            coinSide = `bottom`
+            message = `§8[ §6§l▼§8 ]`
+        }
 
-		Minecraft.getInstance().gameRenderer.displayItemActivation(`cabricality:gold_coin_${coinSide}`)
-		playsound_coin(event.server, player, 'gold')
+        Minecraft.getInstance().gameRenderer.displayItemActivation(`cabricality:gold_coin_${coinSide}`)
+        playsound_coin(event.server, player, 'gold')
 
-		event.server.scheduleInTicks(12, event.server, () => {
-			Minecraft.getInstance().gui.getChat().addMessage(messagePrefix + message)
-			event.server.scheduleInTicks(29, event.server, () => {
-				coinCooling = coinCooling.filter(function(item) { return item != player.toString() })
-			})
-		})
-	}
+        event.server.scheduleInTicks(12, event.server, () => {
+            let messagePrefix = Text.translate(`event.cabricality.coin_flip`, player.getName(), event.getItem().getName()).getString()
+            Minecraft.getInstance().gui.getChat().addMessage(messagePrefix + message)
+            event.server.scheduleInTicks(29, event.server, () => {
+                coinCooling = coinCooling.filter(function(item) { return item != player.toString() })
+            })
+        })
+    }
 })
--- kubejs/server_scripts/dice.js
+++ kubejs/server_scripts/dice.js
@@ -1,36 +1,36 @@
 function playsound_dice(server, player) {
-	server.runCommandSilent(`playsound minecraft:entity.drowned.shoot ambient @a ${player.x} ${player.y} ${player.z}`)
-	server.runCommandSilent(`playsound minecraft:block.deepslate.place ambient @a ${player.x} ${player.y} ${player.z}`)
-	server.runCommandSilent(`playsound minecraft:block.conduit.activate ambient @a ${player.x} ${player.y} ${player.z} 0.7`)
-	server.runCommandSilent(`playsound create:mechanical_press_activation_belt_compounded_1 ambient @a ${player.x} ${player.y} ${player.z} 0.7`)
-	server.runCommandSilent(`playsound minecraft:block.amethyst_cluster.hit ambient @a ${player.x} ${player.y} ${player.z} 1 0.7`)
-	server.runCommandSilent(`playsound ad_astra:passing_spaceship ambient @a ${player.x} ${player.y} ${player.z} 0.3`)
+    server.runCommandSilent(`playsound minecraft:entity.drowned.shoot ambient @a ${player.x} ${player.y} ${player.z}`)
+    server.runCommandSilent(`playsound minecraft:block.deepslate.place ambient @a ${player.x} ${player.y} ${player.z}`)
+    server.runCommandSilent(`playsound minecraft:block.conduit.activate ambient @a ${player.x} ${player.y} ${player.z} 0.7`)
+    server.runCommandSilent(`playsound create:mechanical_press_activation_belt_compounded_1 ambient @a ${player.x} ${player.y} ${player.z} 0.7`)
+    server.runCommandSilent(`playsound minecraft:block.amethyst_cluster.hit ambient @a ${player.x} ${player.y} ${player.z} 1 0.7`)
+    server.runCommandSilent(`playsound ad_astra:passing_spaceship ambient @a ${player.x} ${player.y} ${player.z} 0.3`)
 }
 
 var diceCooling = []
 
 onEvent('block.right_click', event => {
-	let player = event.getEntity()
-	let messagePrefix = Text.translate(`event.cabricality.dice_roll`, player.getName(), event.getBlock().getItem().getName()).getString()
+    let player = event.getEntity()
 
-	if (event.getBlock() == 'cabricality:dice'
-	&& player.getId() != '9e2faded-cafe-4ec2-c314-dad129ae971d'
-	&& player.getMainHandItem() == null) {
-		event.cancel()
-		if (!diceCooling.includes(player.toString())) {
-			diceCooling.push(player.toString())
+    if (event.getBlock() == 'cabricality:dice' &&
+        player.getId() != '9e2faded-cafe-4ec2-c314-dad129ae971d' &&
+        player.getMainHandItem() == null) {
+        event.cancel()
+        if (!diceCooling.includes(player.toString())) {
+            diceCooling.push(player.toString())
 
-			let serial = Math.floor(Math.random() * 6) + 1
+            let serial = Math.floor(Math.random() * 6) + 1
 
-			Minecraft.getInstance().gameRenderer.displayItemActivation(`cabricality:dice_${serial}`) 
-			playsound_dice(event.server, player)
+            Minecraft.getInstance().gameRenderer.displayItemActivation(`cabricality:dice_${serial}`)
+            playsound_dice(event.server, player)
 
-			event.server.scheduleInTicks(12, event.server, () => {
-				Minecraft.getInstance().gui.getChat().addMessage(messagePrefix + `§8[ §5§l${serial}§8 ]`)
-				event.server.scheduleInTicks(35, event.server, () => {
-					diceCooling = diceCooling.filter(function(item) { return item != player.toString() })
-				})
-			})
-		}
-	}
+            event.server.scheduleInTicks(12, event.server, () => {
+                let messagePrefix = Text.translate(`event.cabricality.dice_roll`, player.getName(), event.getBlock().getItem().getName()).getString()
+                Minecraft.getInstance().gui.getChat().addMessage(messagePrefix + `§8[ §5§l${serial}§8 ]`)
+                event.server.scheduleInTicks(35, event.server, () => {
+                    diceCooling = diceCooling.filter(function(item) { return item != player.toString() })
+                })
+            })
+        }
+    }
 })
