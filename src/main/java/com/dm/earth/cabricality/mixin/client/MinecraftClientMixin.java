59a42418190fc236e055ce2fc64ea83490dc552b
KrLite <68179735+KrLite@users.noreply.github.com>
2022-12-26 19:45:08 +0800

    Update MinecraftClientMixin.java

--- src/main/java/com/dm/earth/cabricality/mixin/client/MinecraftClientMixin.java
+++ src/main/java/com/dm/earth/cabricality/mixin/client/MinecraftClientMixin.java
@@ -2,43 +2,36 @@ package com.dm.earth.cabricality.mixin.client;
 
 import java.util.Optional;
 
-import com.dm.earth.cabricality.Cabricality;
-
-import com.dm.earth.cabricality.client.screen.CabfCreditsScreen;
-
-import net.minecraft.client.option.GameOptions;
-
 import org.quiltmc.loader.api.QuiltLoader;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.dm.earth.cabricality.Cabricality;
 import com.dm.earth.cabricality.client.screen.MissingModScreen;
 import com.dm.earth.cabricality.util.ModDeps;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.sound.SoundManager;
 
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
+public abstract class MinecraftClientMixin {
 	@Shadow
-	@Final
-	public GameOptions options;
+	public abstract SoundManager getSoundManager();
 
 	@Inject(method = "getWindowTitle", at = @At("HEAD"), cancellable = true)
-	private void getWindowTitle(CallbackInfoReturnable<String> cir) {
+	private void modifyWindowTitle(CallbackInfoReturnable<String> cir) {
 		Optional<String> title = QuiltLoader.getModContainer(Cabricality.ID)
 				.map(container -> container.metadata().version().raw());
 		title.ifPresent(t -> cir.setReturnValue("Cabricality " + t)); // If present, set title
 	}
 
 	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
-	private void setScreen(MinecraftClient client, Screen screen) {
+	private void checkMods(MinecraftClient client, Screen screen) {
 		if (!ModDeps.isAllLoaded())
 			// If not full loaded, set screen to MissingModScreen
 			client.setScreen(
