5455e96daac20b754819f807e99af46e54a37fe3
Yjn024 <jiening.yu@outlook.com>
2022-12-29 20:39:14 +0800

    and a hand command
    copied from freaworld, but still made by me :laughing:

--- src/main/java/com/dm/earth/cabricality/client/listener/ClientCommandRegistryListener.java
+++ src/main/java/com/dm/earth/cabricality/client/listener/ClientCommandRegistryListener.java
@@ -4,7 +4,7 @@ import static org.quiltmc.qsl.command.api.client.ClientCommandManager.literal;
 
 import org.quiltmc.qsl.command.api.client.ClientCommandRegistrationCallback;
 import org.quiltmc.qsl.command.api.client.QuiltClientCommandSource;
-
+import com.dm.earth.cabricality.client.command.HeldItemInfoCommand;
 import com.dm.earth.cabricality.content.trading.quest.command.BumpQuestCommand;
 import com.mojang.brigadier.CommandDispatcher;
 
@@ -12,6 +12,7 @@ public class ClientCommandRegistryListener implements ClientCommandRegistrationC
 	@Override
 	public void registerCommands(CommandDispatcher<QuiltClientCommandSource> dispatcher) {
 		dispatcher.register(literal("cabricalityClient")
-				.then(literal("dumpTradingQuests").executes(new BumpQuestCommand())));
+				.then(literal("dumpTradingQuests").executes(new BumpQuestCommand()))
+				.then(literal("handHeld").executes(new HeldItemInfoCommand())));
 	}
 }
