0c03f3cf3bbaff2091d4b04e34af7771c5aabdfe
Yjn024 <jiening.yu@outlook.com>
2022-12-29 11:40:33 +0800

    mixin config plugin

--- src/main/java/com/dm/earth/cabricality/content/core/threads/FluixThread.java
+++ src/main/java/com/dm/earth/cabricality/content/core/threads/FluixThread.java
@@ -8,15 +8,28 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.quiltmc.qsl.recipe.api.RecipeLoadingEvents.AddRecipesCallback;
+import org.quiltmc.qsl.recipe.api.RecipeLoadingEvents.RemoveRecipesCallback;
 import org.quiltmc.qsl.recipe.api.builder.VanillaRecipeBuilders;
 import com.dm.earth.cabricality.content.core.TechThread;
+import com.dm.earth.cabricality.content.entries.CabfFluids;
 import com.dm.earth.cabricality.content.entries.CabfItems;
 import com.dm.earth.cabricality.resource.data.core.FreePRP;
+import com.dm.earth.cabricality.tweak.RecipeTweaks;
 import com.dm.earth.cabricality.tweak.core.MechAndSmithCraft;
-import com.dm.earth.cabricality.math.util.RecipeBuilderUtil;
+import com.dm.earth.cabricality.util.math.RecipeBuilderUtil;
+import com.simibubi.create.content.contraptions.components.crusher.CrushingRecipe;
 import com.simibubi.create.content.contraptions.components.deployer.DeployerApplicationRecipe;
-import com.simibubi.create.content.contraptions.components.millstone.MillingRecipe;
+import com.simibubi.create.content.contraptions.components.fan.SplashingRecipe;
+import com.simibubi.create.content.contraptions.components.mixer.CompactingRecipe;
+import com.simibubi.create.content.contraptions.fluids.actors.FillingRecipe;
+import com.simibubi.create.content.contraptions.processing.EmptyingRecipe;
+import com.simibubi.create.content.contraptions.processing.HeatCondition;
+import com.simibubi.create.foundation.fluid.FluidIngredient;
+import io.github.fabricators_of_create.porting_lib.util.FluidStack;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.tag.ItemTags;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -77,25 +90,79 @@ public class FluixThread implements TechThread {
 								Ingredient.ofItems(AE2.asItem("silicon_press")))
 						.keepHeldItem().setResult(AE2.asProcessingOutput("printed_silicon"))));
 
-		handler.register(recipeId("milling", "blizz_cube"),
-				id -> new MillingRecipe(
+		handler.register(recipeId("crushing", "blizz_cube"),
+				id -> new CrushingRecipe(
 						new FreePRP(id).setIngredient(CABF.asIngredient("blizz_cube"))
 								.setResult(CABF.asProcessingOutput("blizz_powder", 1, 2),
 										CABF.asProcessingOutput("blizz_powder", 0.5f, 1))
 								.setProcessingTime(350)));
 
-								handler.register(recipeId("milling", "basalz_powder"),
-								id -> new MillingRecipe(
-										new FreePRP(id).setIngredient(CABF.asIngredient("basalz_shard"))
-												.setResult(CABF.asProcessingOutput("basalz_powder", 1, 2),
-														CABF.asProcessingOutput("basalz_powder", 0.5f, 1))
-												.setProcessingTime(350)));
+		handler.register(recipeId("crushing", "basalz_shard"),
+				id -> new CrushingRecipe(
+						new FreePRP(id).setIngredient(CABF.asIngredient("basalz_shard"))
+								.setResult(CABF.asProcessingOutput("basalz_powder", 1, 2),
+										CABF.asProcessingOutput("basalz_powder", 0.5f, 1))
+								.setProcessingTime(350)));
+
+		var blizz = CABF.asIngredient("blizz_powder");
+		var basalz = CABF.asIngredient("basalz_powder");
+		handler.register(recipeId("splashing", "sandstone"),
+				id -> new SplashingRecipe(
+						new FreePRP(id).setIngredient(MC.asIngredient("sandstone"))
+								.setResult(CABF.asProcessingOutput("sand_ball", 0.125f))));
+		handler.register(recipeId("compacting", "ice_charge"),
+				id -> new CompactingRecipe(new FreePRP(id)
+						.setIngredient(blizz, blizz, blizz, blizz, blizz, blizz, blizz, blizz)
+						.setResult(CABF.asProcessingOutput("ice_charge"))));
+		handler.register(recipeId("compacting", "earth_charge"),
+				id -> new CompactingRecipe(
+						new FreePRP(id)
+								.setIngredient(basalz, basalz, basalz, basalz, basalz, basalz,
+										basalz, basalz)
+								.setResult(CABF.asProcessingOutput("earth_charge"))));
+
+		// Coke Processing
+		handler.register(recipeId("compacting", "coal"),
+				id -> new CompactingRecipe(new FreePRP(id)
+						.setIngredient(Ingredient.ofTag(ItemTags.COALS))
+						.setFluidResult(new FluidStack(CabfFluids.COKE, FluidConstants.INGOT))
+						.setHeatRequirement(HeatCondition.HEATED)));
+		handler.register(recipeId("filling", "coal"),
+				id -> new FillingRecipe(new FreePRP(id)
+						.setIngredient(Ingredient.ofTag(ItemTags.COALS))
+						.setFluidIngredient(
+								FluidIngredient.fromFluid(CabfFluids.COKE, FluidConstants.BOTTLE))
+						.setResult(CABF.asProcessingOutput("coal_coke"))));
+		handler.register(recipeId("compacting", "coal_coke"),
+				id -> new CompactingRecipe(new FreePRP(id)
+						.setIngredient(CABF.asIngredient("coal_coke"))
+						.setFluidIngredient(
+								FluidIngredient.fromFluid(Fluids.WATER, FluidConstants.INGOT))
+						.setResult(MC.asProcessingOutput("coal"))));
+
+		handler.register(recipeId("emptying", "sand_ball"),
+				id -> new EmptyingRecipe(new FreePRP(id)
+						.setIngredient(CABF.asIngredient("sand_ball"))
+						.setResult(CABF.asProcessingOutput("rough_sand")).setFluidResult(
+								new FluidStack(CabfFluids.FINE_SAND, FluidConstants.BUCKET / 2))));
+		handler.register(recipeId("compacting", "silicon_compound"),
+				id -> new CompactingRecipe(new FreePRP(id)
+						.setIngredient(CABF.asIngredient("purified_sand"),
+								CABF.asIngredient("coke_chunk"))
+						.setFluidIngredient(FluidIngredient.fromFluid(CabfFluids.FINE_SAND,
+								FluidConstants.BOTTLE))
+						.setResult(CABF.asProcessingOutput("silicon_compound"))));
 	}
 
 	@Contract("_, _, _ -> new")
 	private MechAndSmithCraft.@NotNull Entry entry(Identifier output, int count,
 			@Nullable Identifier other) {
 		return MechAndSmithCraft.entry(this.getLevel(), AE2.id("controller"), output, count, other);
+	}
+
+	@Override
+	public void removeRecipes(RemoveRecipesCallback.RecipeHandler handler) {
+		handler.removeIf(r -> RecipeTweaks.notCabf(r) && r.getOutput().isOf(AE2.asItem("silicon")));
 	}
 
 }
