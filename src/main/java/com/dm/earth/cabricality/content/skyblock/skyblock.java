6c83dbd0a31aa123a6d9e358e4dd34520015d040
IcedDog <34804287+IcedDog@users.noreply.github.com>
2022-12-30 08:57:46 +0800

    why no cap

--- src/main/java/com/dm/earth/cabricality/content/skyblock/skyblock.java
+++ src/main/java/com/dm/earth/cabricality/content/skyblock/skyblock.java
@@ -1,7 +1,5 @@
 package com.dm.earth.cabricality.content.skyblock;
 
-import com.dm.earth.cabricality.util.debug.CabfLogger;
-
 public class Skyblock {
 	public static Boolean isSkyblock = true;
 	public static void load(){
