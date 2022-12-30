1db230d6fb13aac3c85da43a50a695336b6aff60
KrLite <68179735+KrLite@users.noreply.github.com>
2022-12-30 23:19:27 +0800

    public

--- src/main/java/com/dm/earth/cabricality/math/Node.java
+++ src/main/java/com/dm/earth/cabricality/math/Node.java
@@ -5,18 +5,18 @@ public record Node(double x, double y) {
 		this(0, 0);
 	}
 
-	double getCross(Node p1, Node p2) {
+	public double getCross(Node p1, Node p2) {
 		return (p2.x - p1.x) * (this.y - p1.y) - (this.x - p1.x) * (p2.y - p1.y);
 	}
 
-	Node rotate(Node origin, double clockwiseDegree) {
+	public Node rotate(Node origin, double clockwiseDegree) {
 		return new Node(
 				(this.x - origin.x) * Math.cos(Math.toRadians(clockwiseDegree)) - (this.y - origin.y) * Math.sin(Math.toRadians(clockwiseDegree)) + origin.x,
 				(this.x - origin.x) * Math.sin(Math.toRadians(clockwiseDegree)) + (this.y - origin.y) * Math.cos(Math.toRadians(clockwiseDegree)) + origin.y
 		);
 	}
 
-	Node append(Node node) {
+	public Node append(Node node) {
 		return new Node(this.x + node.x, this.y + node.y);
 	}
 
