36209adba4310d4af7072a3ead0a72424a261581
JieningYu <61787533+JieningYu@users.noreply.github.com>
2023-01-11 16:10:49 +0800

    based

--- src/main/java/com/dm/earth/cabricality/mixin/ftbquests/QuestPanelAnimator.java
+++ src/main/java/com/dm/earth/cabricality/mixin/ftbquests/QuestPanelAnimator.java
@@ -1,17 +1,13 @@
 package com.dm.earth.cabricality.mixin.ftbquests;
 
-import java.util.Collection;
-
-import com.dm.earth.cabricality.Cabricality;
-import dev.ftb.mods.ftblibrary.icon.Color4I;
-import dev.ftb.mods.ftbquests.quest.QuestObjectBase;
-import dev.ftb.mods.ftbquests.quest.theme.property.ColorProperty;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
-import dev.ftb.mods.ftbquests.gui.quests.QuestButton;
+import com.dm.earth.cabricality.Cabricality;
+import dev.ftb.mods.ftblibrary.icon.Color4I;
 import dev.ftb.mods.ftbquests.gui.quests.QuestPanel;
+import dev.ftb.mods.ftbquests.quest.QuestObjectBase;
+import dev.ftb.mods.ftbquests.quest.theme.property.ColorProperty;
 
 @Mixin(QuestPanel.class)
 public class QuestPanelAnimator {
