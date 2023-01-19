8a047389bde56d0a9889bf87bbf53abc7c8928bc
KrLite <68179735+KrLite@users.noreply.github.com>
2023-01-19 12:04:50 +0800

    add DEBUG_INFO networking key

--- src/main/java/com/dm/earth/cabricality/networking/CabfNetworking.java
+++ src/main/java/com/dm/earth/cabricality/networking/CabfNetworking.java
@@ -4,9 +4,11 @@ import com.dm.earth.cabricality.Cabricality;
 import net.krlite.equator.util.IdentifierBuilder;
 import net.minecraft.util.Identifier;
 
+// Networking keys
 public class CabfNetworking {
 	private static final IdentifierBuilder.Specified ID_BUILDER = new IdentifierBuilder.Specified(Cabricality.ID + "_networking");
 
 	// Commands
 	public static final Identifier HELD_ITEM_INFO = ID_BUILDER.id("command", "held_item_info");
+	public static final Identifier DEBUG_INFO = ID_BUILDER.id("command", "debug_info");
 }
