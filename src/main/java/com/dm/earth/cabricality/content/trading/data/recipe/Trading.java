8f5c2440c6b2b1977e949a16712d56d41fceb169
Yjn024 <61787533+JieningYu@users.noreply.github.com>
2023-02-02 16:19:51 +0800

    stop using stupid json for infusing recipes

--- src/main/java/com/dm/earth/cabricality/content/trading/data/recipe/Trading.java
+++ src/main/java/com/dm/earth/cabricality/content/trading/data/recipe/Trading.java
@@ -1,41 +1,46 @@
 package com.dm.earth.cabricality.content.trading.data.recipe;
 
-import com.dm.earth.cabricality.Cabricality;
+import static com.dm.earth.cabricality.ModEntry.CABF;
 
 import org.quiltmc.qsl.recipe.api.RecipeLoadingEvents;
 import org.quiltmc.qsl.recipe.api.RecipeLoadingEvents.AddRecipesCallback;
 import org.quiltmc.qsl.recipe.api.RecipeLoadingEvents.RemoveRecipesCallback;
 
+import com.dm.earth.cabricality.Cabricality;
 import com.dm.earth.cabricality.content.trading.Professions;
 import com.dm.earth.cabricality.content.trading.core.Profession;
 import com.dm.earth.cabricality.content.trading.core.TradingEntry;
-import com.dm.earth.cabricality.content.trading.data.recipe.gen.JTradingRecipeGenerator;
 import com.dm.earth.cabricality.content.trading.data.tag.TradeTags;
+import com.dm.earth.cabricality.content.trading.util.ProfessionUtil;
 import com.dm.earth.cabricality.lib.util.debug.CabfDebugger;
 
+import me.steven.indrev.recipes.machines.InfuserRecipe;
+import me.steven.indrev.recipes.machines.entries.InputEntry;
+import me.steven.indrev.recipes.machines.entries.OutputEntry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Ingredient;
-import net.minecraft.recipe.RecipeManager;
 import net.minecraft.recipe.ShapelessRecipe;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.registry.Registry;
 
 public class Trading implements AddRecipesCallback, RemoveRecipesCallback {
+
 	@Override
 	public void addRecipes(RecipeLoadingEvents.AddRecipesCallback.RecipeHandler handler) {
+		System.out.println("trading");
 		for (Professions rawProfession : Professions.values()) {
 			Profession profession = rawProfession.get();
 			Item professionCard = Registry.ITEM.get(Cabricality.id("profession_card_" + profession.hashString()));
 			handler.register(Cabricality.id("crafting/dupe/card/profession_card/" + profession.hashString()),
 					id -> genDupeRecipe(professionCard, id));
 			for (TradingEntry entry : profession.entries()) {
-				//CabfDebugger.debug("Registering recipe handler: " + entry.hashString());
+				System.out.println("Registering recipe handler: " + entry.hashString());
 				handler.register(Cabricality.id("trading/buy/" + profession.hashString() + "/" + entry.hashString()),
-						id -> RecipeManager.deserialize(id, JTradingRecipeGenerator.generateBuy(entry)));
+						id -> generateBuy(id, entry));
 				handler.register(Cabricality.id("trading/sell/" + profession.hashString() + "/" + entry.hashString()),
-						id -> RecipeManager.deserialize(id, JTradingRecipeGenerator.generateSell(entry)));
+						id -> generateSell(id, entry));
 
 				Item tradeCard = Registry.ITEM.get(Cabricality.id("trade_card_" + entry.hashString()));
 				handler.register(Cabricality.id("crafting/dupe/card/trade_card/" + entry.hashString()),
@@ -48,7 +53,7 @@ public class Trading implements AddRecipesCallback, RemoveRecipesCallback {
 	public void removeRecipes(RecipeLoadingEvents.RemoveRecipesCallback.RecipeHandler handler) {
 		CabfDebugger.debug("Removing infuse recipes!");
 		handler.removeIf(Registry.RECIPE_TYPE.get(new Identifier("indrev", "infuse")),
-				p -> !p.getId().getNamespace().equals("cabricality"));
+				p -> !CABF.checkContains(p));
 	}
 
 	private static ShapelessRecipe genDupeRecipe(Item item, Identifier id) {
@@ -61,4 +66,29 @@ public class Trading implements AddRecipesCallback, RemoveRecipesCallback {
 		Professions.load();
 		TradeTags.load();
 	}
+
+	private static InfuserRecipe generateSell(Identifier id, TradingEntry entry) {
+		InputEntry[] inputs = { new InputEntry(Ingredient.ofItems(entry.getItem()), entry.getItemCount()),
+				new InputEntry(
+						Ingredient.ofItems(
+								CABF.asItem("profession_card_" + ProfessionUtil.fromTradingEntry(entry).hashString())),
+						0) };
+
+		OutputEntry[] outputs = { new OutputEntry(new ItemStack(entry.getCoin(), entry.getCoinCount()), 1) };
+
+		return new InfuserRecipe(id, inputs, outputs, 120);
+	}
+
+	private static InfuserRecipe generateBuy(Identifier id, TradingEntry entry) {
+		InputEntry[] inputs = { new InputEntry(Ingredient.ofItems(entry.getCoin()), entry.getCoinCount()),
+				new InputEntry(
+						Ingredient.ofItems(
+								CABF.asItem("profession_card_" + ProfessionUtil.fromTradingEntry(entry).hashString())),
+						0) };
+
+		OutputEntry[] outputs = { new OutputEntry(new ItemStack(entry.getItem(), entry.getItemCount()), 1) };
+
+		return new InfuserRecipe(id, inputs, outputs, 120);
+	}
+
 }
