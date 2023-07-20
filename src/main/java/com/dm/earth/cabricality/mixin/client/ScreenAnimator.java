c34f4659246e4d4cce575b55595365547c606271
KrLite <68179735+KrLite@users.noreply.github.com>
2023-07-20 15:08:20 +0800

    landing

--- src/main/java/com/dm/earth/cabricality/mixin/client/ScreenAnimator.java
+++ src/main/java/com/dm/earth/cabricality/mixin/client/ScreenAnimator.java
@@ -7,7 +7,6 @@ import net.krlite.equator.render.frame.FrameInfo;
 import net.krlite.equator.visual.animation.animated.AnimatedDouble;
 import net.krlite.equator.visual.color.AccurateColor;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -17,28 +16,56 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(Screen.class)
+public class ScreenAnimator {
+	@Unique
+	private static final AnimatedDouble opacity = new AnimatedDouble(0, 1, 450, Curves.Exponential.Quadratic.OUT);
+
+	@ModifyArgs(
+			method = "renderBackground(Lnet/minecraft/client/util/math/MatrixStack;I)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/gui/screen/Screen;fillGradient(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"
+			)
+	)
+	private void fadeIn(Args args) {
+		PushUtil.SCREEN.pull(opacity::replay);
+
+		int upper = args.get(5), lower = args.get(6);
+
+		args.set(5, AccurateColor.fromARGB(upper).opacity(opacity.value()).toInt());
+		args.set(6, AccurateColor.fromARGB(lower).opacity(opacity.value()).toInt());
+	}
+}
 
 @Mixin(GameRenderer.class)
-public class ScreenAnimator {
+class PostScreenAnimator {
 	@Unique
 	private static final AnimatedDouble opacity = new AnimatedDouble(1, 0, 450, Curves.Exponential.Quadratic.OUT);
 
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;render(Lnet/minecraft/client/util/math/MatrixStack;F)V"))
-	private void renderVignette(InGameHud inGameHud, MatrixStack matrixStack, float tickDelta) {
+	@Inject(
+			method = "render",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/gui/hud/InGameHud;render(Lnet/minecraft/client/util/math/MatrixStack;F)V",
+					shift = At.Shift.AFTER
+			)
+	)
+	private void fadeOut(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
 		PushUtil.POST_SCREEN.pull(opacity::replay);
 
 		if (opacity.isPlaying()) {
-			FrameInfo.scaled().render(matrixStack,
+			FrameInfo.scaled().render(new MatrixStack(),
 					flat -> flat.new Rectangle()
 									.colorTop(AccurateColor.fromARGB(0xC0101010L))
 									.colorBottom(AccurateColor.fromARGB(0xD0101010L))
 									.opacityMultiplier(opacity.value())
 			);
 		}
-
-		inGameHud.render(matrixStack, tickDelta);
 	}
 }
 
@@ -50,10 +77,7 @@ class PostScreenTriggerer {
 
 	@Inject(method = "setScreen", at = @At("HEAD"))
 	private void setScreen(@Nullable Screen screen, CallbackInfo ci) {
-		if (
-				screen == null && (currentScreen != null && !ScreenUtil.isUnextendedScreen(currentScreen.getClass()))
-		) {
-			PushUtil.POST_SCREEN.push();
-		}
+		if (screen != null && !ScreenUtil.isUnextendedScreen(screen.getClass())) PushUtil.SCREEN.push();
+		if (screen == null && (currentScreen != null && !ScreenUtil.isUnextendedScreen(currentScreen.getClass()))) PushUtil.POST_SCREEN.push();
 	}
 }
