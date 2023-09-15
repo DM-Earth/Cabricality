dff152291661667d359c507aca08ea753879865c
KrLite <68179735+KrLite@users.noreply.github.com>
2023-09-15 22:59:30 +0800

    better scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,6 @@ repositories {
     }
 
     maven {
-        name = "Ladysnake Mods"
         url = uri("https://maven.ladysnake.org/releases")
         content {
             includeGroup("io.github.ladysnake")
@@ -52,20 +51,20 @@ repositories {
         }
     }
 
-    maven { url = uri("https://mvn.devos.one/snapshots/") }
-    maven { url = uri("https://maven.parchmentmc.org") }
-    maven { url = uri("https://maven.fabricmc.net/") }
-    maven { url = uri("https://storage.googleapis.com/devan-maven/") }
-    maven { url = uri("https://maven.tterrag.com/") }
-    maven { url = uri("https://maven.shedaniel.me/") }
     maven { url = uri("https://aperlambda.github.io/maven") }
-    maven { url = uri("https://maven.kotlindiscord.com/repository/terraformers/") }
-    maven { url = uri("https://maven.terraformersmc.com/releases/") }
-    maven { url = uri("https://maven.cafeteria.dev/releases/") }
-    maven { url = uri("https://maven.gegy.dev") }
     maven { url = uri("https://dvs1.progwml6.com/files/maven/") }
+    maven { url = uri("https://maven.cafeteria.dev/releases/") }
+    maven { url = uri("https://maven.fabricmc.net/") }
+    maven { url = uri("https://maven.gegy.dev") }
+    maven { url = uri("https://maven.kotlindiscord.com/repository/terraformers/") }
+    maven { url = uri("https://maven.parchmentmc.org") }
+    maven { url = uri("https://maven.shedaniel.me/") }
+    maven { url = uri("https://maven.terraformersmc.com/releases/") }
+    maven { url = uri("https://maven.tterrag.com/") }
     maven { url = uri("https://maven.wispforest.io") }
     maven { url = uri("https://modmaven.dev") }
+    maven { url = uri("https://mvn.devos.one/snapshots/") }
+    maven { url = uri("https://storage.googleapis.com/devan-maven/") }
 
     mavenCentral()
 }
@@ -86,60 +85,60 @@ dependencies {
     // Create
     modImplementation(
         "com.simibubi.create:create-fabric-" +
-                "${project.properties["minecraft_version"]}" + ":" +
-                "${project.properties["create_version"]}" +
-                "+mc" + "${project.properties["minecraft_version"]}"
+                "${property("minecraft_version")}" + ":" +
+                "${property("create_version")}" +
+                "+mc" + "${property("minecraft_version")}"
     ) { exclude(group = "com.github.AlphaMode") }
 
     // Implemented Mods
     // - Miscellaneous
     modImplementation(
         "slimeknights.mantle:Mantle:" +
-                "${project.properties["minecraft_version"]}" + "-" +
-                "${project.properties["mantle_version"]}"
+                "${property("minecraft_version")}" + "-" +
+                "${property("mantle_version")}"
     ) { exclude(group = "com.github.AlphaMode") }
 
     // - Modrinth Maven
-    modImplementation("maven.modrinth:ad-astra:${project.properties["ad_astra_version"]}")
-    modImplementation("maven.modrinth:architectury-api:${project.properties["architectury_version"]}+fabric")
-    modImplementation("maven.modrinth:bits-and-chisels:${project.properties["bits_and_chisels_version"]}")
-    modImplementation("maven.modrinth:farmers-delight-fabric:${project.properties["farmers_delight_version"]}")
-    modImplementation("maven.modrinth:led:${project.properties["led_version"]}")
-    modImplementation("maven.modrinth:interference:${project.properties["interference_version"]}")
+    modImplementation("maven.modrinth:ad-astra:${property("ad_astra_version")}")
+    modImplementation("maven.modrinth:architectury-api:${property("architectury_version")}+fabric")
+    modImplementation("maven.modrinth:bits-and-chisels:${property("bits_and_chisels_version")}")
+    modImplementation("maven.modrinth:farmers-delight-fabric:${property("farmers_delight_version")}")
+    modImplementation("maven.modrinth:led:${property("led_version")}")
+    modImplementation("maven.modrinth:interference:${property("interference_version")}")
 
     // - Curse Maven
-    modImplementation("curse.maven:industrial-revolution-391708:${project.properties["indrev_version"]}")
-    modImplementation("curse.maven:ftb-quests-fabric-438496:${project.properties["ftb_quests_version"]}")
-    modImplementation("curse.maven:ftb-library-fabric-438495:${project.properties["ftb_library_version"]}")
-    modImplementation("curse.maven:catwalks-inc-554340:${project.properties["catwalks_version"]}")
-    modImplementation("curse.maven:malum-quilt-627875:${project.properties["malum_version"]}")
-    implementation("curse.maven:hephaestus-667894:${project.properties["hephaestus_version"]}") // Toxic
+    modImplementation("curse.maven:industrial-revolution-391708:${property("indrev_version")}")
+    modImplementation("curse.maven:ftb-quests-fabric-438496:${property("ftb_quests_version")}")
+    modImplementation("curse.maven:ftb-library-fabric-438495:${property("ftb_library_version")}")
+    modImplementation("curse.maven:catwalks-inc-554340:${property("catwalks_version")}")
+    modImplementation("curse.maven:malum-quilt-627875:${property("malum_version")}")
+    implementation("curse.maven:hephaestus-667894:${property("hephaestus_version")}") // Toxic
 
     // - JitPack
-    modImplementation("com.github.KrLite.Equator-v2:build:${project.properties["minecraft_major_version"]}-${project.properties["equator_version"]}")
+    modImplementation("com.github.KrLite.Equator-v2:build:${property("minecraft_major_version")}-${property("equator_version")}")
 
     // Mod Apis
-    modApi("com.terraformersmc:modmenu:${project.properties["modmenu_version"]}")
-    modApi("me.shedaniel.cloth:cloth-config-fabric:${project.properties["cloth_config_version"]}") {
+    modApi("com.terraformersmc:modmenu:${property("modmenu_version")}")
+    modApi("me.shedaniel.cloth:cloth-config-fabric:${property("cloth_config_version")}") {
         exclude(group = "net.fabricmc.fabric-api")
     }
 
     // Included
-    modImplementation(include("com.github.DM-Earth:Tags-Binder:${project.properties["tags_binder_version"]}")!!)
-    modImplementation(include("maven.modrinth:brrp:${project.properties["brrp_version"]}-mc${project.properties["minecraft_version"]}-fabric")!!)
+    modImplementation(include("com.github.DM-Earth:Tags-Binder:${property("tags_binder_version")}")!!)
+    modImplementation(include("maven.modrinth:brrp:${property("brrp_version")}-mc${property("minecraft_version")}-fabric")!!)
 
-    implementation(include("net.objecthunter:exp4j:${project.properties["exp4j_version"]}")!!)
-    implementation(include("com.github.KrLite:Pierced:${project.properties["pierced_version"]}")!!)
-    implementation(include("com.github.davidmoten:word-wrap:${project.properties["word_wrap_version"]}")!!)
-    implementation(include("com.github.davidmoten:guava-mini:${project.properties["guava_mini_version"]}")!!)
+    implementation(include("net.objecthunter:exp4j:${property("exp4j_version")}")!!)
+    implementation(include("com.github.KrLite:Pierced:${property("pierced_version")}")!!)
+    implementation(include("com.github.davidmoten:word-wrap:${property("word_wrap_version")}")!!)
+    implementation(include("com.github.davidmoten:guava-mini:${property("guava_mini_version")}")!!)
 
     // Development
-    compileOnlyApi("org.apiguardian:apiguardian-api:${project.properties["apiguardian_version"]}")
+    compileOnlyApi("org.apiguardian:apiguardian-api:${property("apiguardian_version")}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    modCompileOnly("me.shedaniel:RoughlyEnoughItems-default-plugin-fabric:${project.properties["rei_version"]}")
-    modCompileOnly("me.shedaniel:RoughlyEnoughItems-api-fabric:${project.properties["rei_version"]}")
-    modRuntimeOnly("me.shedaniel:RoughlyEnoughItems-fabric:${project.properties["rei_version"]}")
+    modCompileOnly("me.shedaniel:RoughlyEnoughItems-default-plugin-fabric:${property("rei_version")}")
+    modCompileOnly("me.shedaniel:RoughlyEnoughItems-api-fabric:${property("rei_version")}")
+    modRuntimeOnly("me.shedaniel:RoughlyEnoughItems-fabric:${property("rei_version")}")
 }
 
 tasks {
