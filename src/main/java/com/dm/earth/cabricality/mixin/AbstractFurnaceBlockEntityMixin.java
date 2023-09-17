2b6c1f07f9bf3fd1c29385e34f7ccb890f551204
KrLite <68179735+KrLite@users.noreply.github.com>
2023-09-17 09:35:49 +0800

    mixins fix

--- src/main/java/com/dm/earth/cabricality/mixin/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/com/dm/earth/cabricality/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -3,6 +3,7 @@ package com.dm.earth.cabricality.mixin;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,9 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class AbstractFurnaceBlockEntityMixin {
 	@Inject(method = "craftRecipe", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;increment(I)V", shift = At.Shift.AFTER))
-	private static void fixRecipe(Recipe<?> recipe, DefaultedList<ItemStack> slots, int countIdk, CallbackInfoReturnable<Boolean> cir) {
-		int count = recipe.getOutput().getCount();
-		if (count > 1)
-			slots.get(2).increment(count - 1);
+	private static void fixRecipe(DynamicRegistryManager registryManager, Recipe<?> recipe, DefaultedList<ItemStack> slots, int count, CallbackInfoReturnable<Boolean> cir) {
+		if (count > 1) slots.get(2).increment(count - 1);
 	}
 }
